54a7e2a8b5d3c71cd1e195ebd8a65d5444d27584
Hamilton Ceballos <hamilton.ceballos686@asys.edu.co>
2024-08-29 20:43:01 -0500

    tarea1

---
+++ semana VI/comentarios/index.js
@@ -0,0 +1,6 @@
+//este es un comentario de una sola linea//
+/* esto
+es
+un comentario de
+multiples lineas
+ */
---
+++ semana v/1 variables y constantes/index.js
@@ -0,0 +1,22 @@
+console.log("hola mundo");
+console.log("hola como estas")
+console.log("bien gracias")
+console.table({"hola": "mundo"});
+console.table("hola Mundo")
+const prueba = "hola mundo prueba";
+console.log(prueba);
+
+var variablevar = "esto es una variable var"
+//comentario en linea no usar var//
+/* este
+es un
+comentario de bloque*/
+ */
+let variablelet = "esto es una variable let"
+const variablesconsts = "esto es una variable const"
+//siempre que pueda utilizar const//
+
+
+
+
+
